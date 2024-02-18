1c49abeb80705cb63fc6619bf3bbac6c24960cec
Or Cohen <136265306+orscohen@users.noreply.github.com>
2024-02-18 22:45:30 +0200

    Update Automate_test_rules_from_policy.py

--- Carbon-Tools/Automate_test_rules_from_policy.py
+++ Carbon-Tools/Automate_test_rules_from_policy.py
@@ -64,7 +64,7 @@ def create_query(rule):
         "INVOKE_CMD_INTERPRETER": f"(process_name:{value}) AND (childproc_name:cmd.exe OR childproc_name:powershell.exe OR childproc_name:cscript.exe OR childproc_name:wscript.exe OR childproc_name:wmic.exe OR childproc_name:mshta.exe OR childproc_name:sh OR childproc_name:zsh OR childproc_name:csh OR childproc_name:bash OR childproc_name:tcsh OR childproc_name:python)",
         "RUN": f"(process_name:{value})",
         "MEMORY_SCRAPE": f"(process_name:{value}) AND (ttp:RAM_SCRAPING OR ttp:READ_SECURITY_DATA)",
-        "CODE_INJECTION": f"(process_effective_reputation:SUSPECT_MALWARE AND (ttp:INJECT_CODE OR ttp:HAS_INJECTED_CODE OR ttp:COMPROMISED_PROCESS OR ttp:PROCESS_IMAGE_REPLACED OR ttp:MODIFY_PROCESS OR ttp:MODIFY_PROCESS_EXECUTION OR ttp:HOLLOW_PROCESS))",
+        "CODE_INJECTION": f"(process_name:{value}) AND (ttp:INJECT_CODE OR ttp:HAS_INJECTED_CODE OR ttp:COMPROMISED_PROCESS OR ttp:PROCESS_IMAGE_REPLACED OR ttp:MODIFY_PROCESS OR ttp:MODIFY_PROCESS_EXECUTION OR ttp:HOLLOW_PROCESS))",
         "NETWORK": f"(process_name:{value}) AND (netconn_count:[1 TO *] OR ttp:NETWORK_ACCESS OR ttp:ATTEMPTED_SERVER OR ttp:ATTEMPTED_CLIENT)",
         "POL_INVOKE_NOT_TRUSTED": f"(process_name:{value}) AND (childproc_effective_reputation:NOT_LISTED OR childproc_effective_reputation:UNKNOWN OR childproc_effective_reputation:ADAPTIVE_WHITE_LIST)",
         "RUN_INMEMORY_CODE": f"(process_name:{value}) AND (ttp:SUSPICIOUS_BEHAVIOR OR ttp:PACKED_CALL)",
